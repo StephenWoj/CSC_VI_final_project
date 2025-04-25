93f7bc071ef32b6cd584a0662e53f4f4e5a81013
StephenWoj <swojtanowski@my.madonna.edu>
2025-04-25 15:26:40 -0400

    initial commit

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth import login, logout, update_session_auth_hash
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
